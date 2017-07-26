5f9c4ff4e526f830594e6a0e84106bf9bcdcb138
CSX <100401176@qq.com>
2017-07-26 13:04:59 +0800

    'updata'

---
+++ 我的教程步骤/步骤22-JSX样式与CSS的互转/代码/webpack.config.js
@@ -0,0 +1,45 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: path.join(__dirname),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./src/js/index.js",
+  module:{
+    rules:[
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ['react-html-attrs'], //添加组件的插件配置
+        }
+      },
+      //下面是添加的 css 的 loader，也即是 css 模块化的配置方法，大家可以拷贝过去直接使用
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ]
+  },
+  output: {
+    path: __dirname,
+    filename: "./src/bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
---
+++ 我的教程步骤/步骤24-AntDesign样式框架使用/代码/src/js/components/header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ComponentHeader extends React.Component{
+/*
+  创建一个类 ComponentHeader，它是继承于 React.Component 的子类
+  类名必须大写
+*/
+  constructor(){
+  //state想要初始化的话，需要有一个构造函数 constructor()
+    super();
+    //调用基类的所有的初始化方法，比如 state
+    this.state = {
+      miniHeader: false
+      //miniHeader变量 默认为 false
+    }
+  };
+  switchHeader(){
+    this.setState({
+      miniHeader: !this.state.miniHeader
+    })
+  };
+  //每当用户点击时，切换 true/false 状态
+
+  render(){
+    /*内联样式*/
+    const styleComponentHeader = {
+      header:{
+        backgroundColor: "#333333",
+        color: "#FFFFFF",
+        "paddingTop": (this.state.miniHeader) ? "3px" : "15px",
+        paddingBottom: (this.state.miniHeader) ? "3px" : "15px"
+        //判断变量状态，选择相应的值
+      },
+      header2:{
+        backgroundColor: "red",
+        color: "#FFFFFF",
+        "paddingTop": (this.state.miniHeader) ? "3px" : "15px",
+        paddingBottom: (this.state.miniHeader) ? "3px" : "15px"
+        //判断变量状态，选择相应的值
+      },
+      h1Text:{
+        color:"#fff",
+      }
+      //还可以定义其他的样式
+
+    };
+    /*内联样式 结束*/
+    return (
+      <header className="smallFontSize" style={styleComponentHeader.header} onClick={this.switchHeader.bind(this)}>
+      {/*
+        className引用class
+        onClick={this.switchHeader.bind(this)
+        点击触发 switchHeader 函数
+      */}
+        <h1 style={styleComponentHeader.h1Text}>这里是头部</h1>
+      </header>
+    )
+  }
+}
